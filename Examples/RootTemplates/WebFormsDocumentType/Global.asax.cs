e1687e8928d7737abc68ce99c3011dc8e5888555
Ivan Osmak <ivan.osmak@telerik.com>
2012-11-27 15:40:58 +0200

    Implemented sample for WebForms document type changing.

---
+++ Examples/RootTemplates/WebFormsDocumentType/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Sitefinity.PowerTools;
+using SitefinityWebApp.RootTemplates;
+
+namespace SitefinityWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            PowerTools.Instance
+                      .RootTemplates
+                      .RegisterWebFormsTemplateResolver<CustomWebFormsResolver>();
+        }
+        
+    }
+}
---
+++ Examples/RootTemplates/WebFormsDocumentType/RootTemplates/CustomWebFormsResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Web.Hosting;
+using System.Web.Routing;
+using Sitefinity.PowerTools.RootTemplates;
+
+namespace SitefinityWebApp.RootTemplates
+{
+    public class CustomWebFormsResolver : IRootTemplateResolver
+    {
+        public RootTemplate ResolveTemplate(RootTemplate rootTemplate, RequestContext requestContext, string theme)
+        {
+            if (!rootTemplate.IsBackend)
+            {
+                var templatePath = HostingEnvironment.MapPath("~/RootTemplates/WebFormsHtml5.html");
+                return rootTemplate.FromFile(templatePath);
+            }
+
+            return rootTemplate;
+        }
+    }
+}
